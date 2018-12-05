4c6a8ad18712cacef5218f3cef569c24d3d3cb83
ViktorijaJ <viktorija.jakobate@gmail.com>
2018-12-05 10:18:30 +0200

    pabaigtas 10 uzdavinys

--- uzd10.php
+++ uzd10.php
@@ -4,29 +4,49 @@
     include("fragments/styles.php");
     ?>
 </head>
-
 <body>
 <?php
-
 include("fragments/menu.php");
-
-$a = isset ($_REQUEST["a"]) ? $_REQUEST["a"] : "";
-$b = isset ($_REQUEST["b"]) ? $_REQUEST["b"] : "";
+$a = isset($_REQUEST["a"]) ? $_REQUEST["a"] : "";
+$b = isset($_REQUEST["b"]) ? $_REQUEST["b"] : "";
 ?>
-<h2>Pasinaudojant forma parašyti skaičiuotuvą, kuris atlieka dviejų skaičių sumą, skirtumą, sandaugą, dalybą.</h2>
+<>Pasinaudojant forma parašyti skaičiuotuvą, kuris atlieką dviejų skaičių sumą, skirtumą, sandaugą, dalybą. <h2>
 
 <form method="get" action="uzd10.php">
-    <input type="text" name="a" placeholder="Iveskite skaiciaus a reiksme" value="<?php echo $a; ?>">
-    <input type="text" name="b" placeholder="Iveskite skaiciaus b reiksme" value="<?php echo $b; ?>">
 
-    <input type="button">
-    <input type="submit" value="action">
+    <input type="text" name="a" placeholder="Iveskite a" value="<?php echo $a; ?>">
+    <input type="text" name="b" placeholder="Iveskite b" value="<?php echo $b; ?>"><br>
+
+    <input type="radio" name="action" value="+">+
+    <input type="radio" name="action" value="-">-<br>
+    <input type="radio" name="action" value="*">*
+    <input type="radio" name="action" value="/">/<br>
+
+    <input type="submit" value="skaiciuok">
 
 </form>
-<atsakymas> <?php
-    if (isset($_REQUEST["a"]) && isset ($_REQUEST["b"]) && isset ($_REQUEST["action"]))
-
-    ?></atsakymas>
-
+<atsakymas>
+    <?php
+    if (isset($_REQUEST["a"]) && isset($_REQUEST["b"]) && isset($_REQUEST["action"])) {
+        $a = $_REQUEST["a"];
+        $b = $_REQUEST["b"];
+        $action = $_REQUEST["action"];
+        switch ($action) {
+            case "+":
+                echo $a + $b;
+                break;
+            case "-":
+                echo $a - $b;
+                break;
+            case "*":
+                echo $a * $b;
+                break;
+            case "/":
+                echo $a / $b;
+                break;
+        }
+    }
+    ?>
+</atsakymas>
 </body>
 </html>
